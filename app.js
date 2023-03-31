750bfd3ed7c32a1f7bd546ef9b9bc1cfc4c5edc4
carlos guilherme <carlosguilherme1010@gmail.com>
2023-03-31 16:46:30 -0300

    update

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const asteriskManager = require("asterisk-manager");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const path = require("path");
 
+app.use(express.static("views/log"));
+app.set("view engine", "html");
 let ami = asteriskManager(
   "5038",
   "pabx20.brdsoft.com.br",
@@ -23,7 +27,6 @@ app.get("/api/asterisk", async function (req, res) {
     if (evt.event === "QueueStatusComplete") {
       ami.removeListener("managerevent", managerevent);
     } else if (evt.event === "QueueEntry" || evt.event === "QueueParams") {
-      //else if (evt.event === "QueueParams") {
       status.push(evt);
     }
   }
@@ -33,4 +36,57 @@ app.get("/api/asterisk", async function (req, res) {
 
   res.status(200).json(status);
   status = [];
+});
+
+const credentials = {
+  username: "usuario",
+  password: "senha",
+};
+
+// Rota para autenticação
+app.post("/login", (req, res) => {
+  // Verifica se as credenciais estão corretas
+  if (
+    req.body.username === credentials.username &&
+    req.body.password === credentials.password
+  ) {
+    // Cria um token JWT
+    const token = jwt.sign(
+      { username: credentials.username },
+      "MinhaStringSecreta123"
+    );
+
+    // Envia o token como resposta
+    res.json({ token: token });
+  } else {
+    // Retorna um erro de autenticação
+    res.status(401).json({ error: "Credenciais inválidas" });
+  }
+});
+
+// Rota protegida que só pode ser acessada com um token válido
+app.get("/index", authenticateToken, (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+// Middleware para autenticar o token
+function authenticateToken(req, res, next) {
+  // Verifica se o token foi enviado
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  // Verifica se o token é válido
+  jwt.verify(token, "MinhaStringSecreta123", (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    app.get("/", (req, res) => {});
+
+    next();
+  });
+}
+
+app.get("/", (req, res) => {
+  res.render("log_in/login_screen.html");
 });
---
+++ front/graphic.js
@@ -0,0 +1,104 @@
+import "../node_modules/axios/dist/axios.min.js";
+import "../node_modules/chart.js/dist/chart.umd.js";
+
+const getData = async () => {
+  const retrievedData = await axios
+    .get("http://localhost:8001/api/asterisk")
+    .then((res) => res);
+
+  const queueParams = [];
+  const queueEntries = [];
+
+  retrievedData.data.forEach((obj) => {
+    if (obj.hasOwnProperty("queue")) {
+      if (!queueParams[obj.queue]) {
+        queueParams[obj.queue] = [];
+      }
+      queueParams[obj.queue].push(obj);
+    } else {
+      queueEntries.push(obj);
+    }
+  });
+
+  return { queueParams, queueEntries };
+};
+const updateChart = async () => {
+  const data = await getData();
+
+  const queueKeys = Object.keys(data.queueParams);
+
+  const totals = {
+    calls: 0,
+    abandoned: 0,
+    completed: 0,
+  };
+  const dddToState = {
+    68: "AC",
+    82: "AL",
+    92: "AM",
+    71: "BA",
+    88: "CE",
+    61: "DF",
+    27: "ES",
+    62: "GO",
+    98: "MA",
+    65: "MT",
+    67: "MS",
+    31: "MG",
+    91: "PA",
+    83: "PB",
+    41: "PR",
+    81: "PE",
+    22: "PI",
+    21: "RJ",
+    84: "RN",
+    51: "RS",
+    69: "RO",
+    95: "RR",
+    47: "SC",
+    79: "SE",
+    11: "SP",
+    63: "TO",
+  };
+
+  const defaultmap = document.querySelectorAll("path");
+  defaultmap.forEach((defaultmap) => {
+    defaultmap.setAttribute("fill", "black");
+  });
+
+  queueKeys.forEach((key) => {
+    const params = data.queueParams[key];
+    const fact = params[0];
+    let G = 1;
+
+    totals.calls += fact.calls++;
+    totals.abandoned += fact.abandoned++;
+    totals.completed += fact.completed++;
+
+    for (let i = 0; i < params.length - 1; i++) {
+      const geo = params[G];
+      G++;
+
+      const ddd = geo.calleridnum.substr(0, 2);
+      const state = dddToState[ddd];
+
+      if (state) {
+        const element = document.getElementById(`BR-${state}`);
+        element.setAttribute("fill", "rgb(153, 255, 0)");
+      }
+    }
+
+    const aban = `<p class="content" style="font-size: 30px">${totals.abandoned}</p>`;
+    document.querySelector(".aban p").innerHTML = aban;
+
+    const call = `<p class="content" style="font-size: 30px">${totals.calls}</p>`;
+    document.querySelector(".call p").innerHTML = call;
+
+    const complete = `<p class="content" style="font-size: 30px">${totals.completed}</p>`;
+    document.querySelector(".complete p").innerHTML = complete;
+  });
+};
+
+setInterval(updateChart, 1000);
+await updateChart();
+
