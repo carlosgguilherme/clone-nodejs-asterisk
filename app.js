59653bcb8db0ec3cf8abfa773ad3754bbfe2362f
carlosguilherme <carlos.guilherme@brdsoft.com.br>
2023-06-05 09:55:52 -0300

    teste

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ const express = require("express");
 const asteriskManager = require("asterisk-manager");
 
 const { createClient } = require("redis");
+const { resolve } = require("path");
 const Client = createClient();
 
+const getAllProducts = async() => {
+  ami.on("connect", () => {
+    ami.action({ action: "login" }, (err, res) => {
+      if (err) {
+        console.error("Erro ao realizar login:", err);
+      } else {  
+      console.log("Realizei o login")
+      ami.action({ action: "SIPpeers" });
+      ami.on("peerentry", (evt) => {
+        const status = evt.status;
+        const ip = evt.ipaddress;
+        const name = evt.objectname;
+        sipPeers.push({
+          status,
+          ip,
+          name,
+        });
+        resolve(sipPeers)
+      }); 
+}
+console.log(getAllProducts)
 const app = express();
 const ami = asteriskManager("5038", "168.121.6.140", "websul", "bilmaster");
 const startup = async () => {
   ami.connect();
-  app.listen(6969, () => {
+  app.listen(7979, () => {
     console.log("Conectei na porta 6969");
   });
 };
@@ -16,26 +38,14 @@ startup();
 
 let sipPeers = [];
 
-ami.on("connect", () => {
-  ami.action({ action: "login" }, (err, res) => {
-    if (err) {
-      console.error("Erro ao realizar login:", err);
-    } else {
+
       app.get("/sippeers", async function (req, res) {
-        ami.action({ action: "SIPpeers" });
-        ami.on("peerentry", (evt) => {
-          const status = evt.status;
-          const ip = evt.ipaddress;
-          const name = evt.objectname;
-          sipPeers.push({
-            status,
-            ip,
-            name,
-          });
-        }); 
-        res.status(200).json(sipPeers);
+        const products = await getAllProducts();
+        res.send('ksakdsakdsak')
+       
         ami.action({ action: "Logoff" });
       });
     }
-  });
-});
+    )}
+   )}
+
